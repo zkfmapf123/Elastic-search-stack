0e5be882506cfdf79afce3de6cf8f1fe9b9ef28d
leedonggyu <zkfmapf123@github.com>
2024-01-06 23:04:16 +0900

    Add to sh file

--- cloud/user_data.sh
+++ cloud/user_data.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+## update
 sudo yum update -y
+
+## docker
+sudo yum install docker -y
+sudo usermod -aG docker $USER
+sudo newgrp docker
+sudo systemctl restart docker
+
+## docker-compose 
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose ## symbolic 링크생성
